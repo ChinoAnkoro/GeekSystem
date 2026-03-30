b8bd5c03fb07f215ea1be714d9bb6e340a3ea0e6
chino <yahooanton8@gmail.com>
2026-03-30 15:32:13 +0900

    OrderHistoryRepository修正

--- geek_system/src/main.java/com/example/demo/repository/OrderHistoryRepository.java
+++ geek_system/src/main.java/com/example/demo/repository/OrderHistoryRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import com.example.demo.dto.OrderHistoryDTO;
 import com.example.demo.entity.OrderHistory;
 
 public interface OrderHistoryRepository extends JpaRepository<OrderHistory, Integer> {
@@ -24,5 +25,5 @@ public interface OrderHistoryRepository extends JpaRepository<OrderHistory, Inte
 			JOIN o.shop s
 			WHERE o.shopId = :shopId
 			""")
-	List<OrderHistory> findByShopId(Integer shopId);
+	List<OrderHistoryDTO> findByShopId(Integer shopId);
 }
